e009ef1de695d1e2648720bfdbbad7a3fb851885
JM\jm538 <optimus5383@gmail.com>
2024-01-26 12:11:48 +0900

    s

--- Sensitivity_analysis/Boruta.py
+++ Sensitivity_analysis/Boruta.py
@@ -15,29 +15,26 @@ if __name__ == "__main__":
         config = parser.parse_args()
 
         today= datetime.today()
-        Data_file = 'C:/Users/User/Desktop/Code/TV/Data/2024-01-09/Data.csv'
+        Data_file = 'C:/Users/jm538/Desktop/Code/TV/Data/ML/Custom_data/concat.csv'
         data = pd.read_csv(Data_file,header=0)
-
-        y1 = data.pop('Car.FyFR')
-        y2 = data.pop('Car.FyFL')
-        y3 = data.pop('Car.FyRL')
-        y4 = data.pop('Car.FyRR')
-
-        y = y1 + y2 + y3 + y4
+        data = data.sample(40000)
+        My = data.pop('My')
 
         hint = np.zeros(len(data.columns))
         sum_importances = np.zeros(len(data.columns))
+        print("start")
+        print(len(My))
         for i in range(config.iteration):
             start = time.time()
             np.random.seed(i + config.start)
-            print('Cal_FyFR', i)
             X_shadow = data.apply(np.random.permutation)
             X_shadow.columns = ['shuffled_' + feat for feat in data.columns]
 
             X_boruto = pd.concat([data, X_shadow], axis=1)
 
-            mdl = RandomForestRegressor(max_depth=6)
-            mdl.fit(X_boruto, y)
+            mdl = RandomForestRegressor(max_depth=5
+                                        )
+            mdl.fit(X_boruto, My)
             feature_imp_x = mdl.feature_importances_[:len(data.columns)]
             feature_imp_shuffled = mdl.feature_importances_[len(data.columns):]
             sum_importances += mdl.feature_importances_[:len(data.columns)]
@@ -48,7 +45,7 @@ if __name__ == "__main__":
         usable_data1 = pd.DataFrame({'hints': hint,'importances':sum_importances}, index = data.columns.to_list())
 
 
-        with pd.ExcelWriter(os.path.join('Data','Test.xlsx'), engine="xlsxwriter") as writer:
+        with pd.ExcelWriter(os.path.join('Data','Test2.xlsx'), engine="xlsxwriter") as writer:
                     usable_data1.to_excel(writer, sheet_name="variables")
         print('Program End')
 
